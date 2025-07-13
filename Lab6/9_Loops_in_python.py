28485d74941c903519d12cc6ae40310d6c61b582
david-tsankov <david.tsankov@gmail.com>
2025-07-13 17:04:18 +0300

    q1 from loops hw

--- Lab6/9_Loops_in_python.py
+++ Lab6/9_Loops_in_python.py
@@ -1,9 +1,17 @@
-# ---------------------------------- Task 1 ---------------------------------- #
+# legend: ✅-got correct output
+#         ❌-couldnt get correct output 
+
+# ---------------------------------- Task 1 ---------------------------------- #✅
 """ DESCRIPTION:
     Write a program that takes an integer n and prints a triangle pattern of stars (*). The number of stars in the first line is 1, in the second line is 2, and so on up to n stars in the n-th line.
 """
 
 ### Your code here
+
+# int=int(input("Please enter an integer: "))
+# index=0
+# for i in range(1,int+1):
+#     print(f"{i*"*"}")
 
 ### EXPECTED OUTPUT:
 # Enter stars number: 4
