78a181070efac334f2a7e31bbc228aa033114a56
david-tsankov <david.tsankov@gmail.com>
2025-07-22 23:51:22 +0200

    will deal with spaghetti tomorrow, started guess the number

---
+++ guess_the_number/guess_the_number.py
@@ -0,0 +1,37 @@
+'''
+Task: Create a Python program for a "Guess the Number" game. The game will generate a random number within a specified range and prompt the user to guess the number. The program provides feedback based on the user's guess - whether it's too high, too low, or correct. The game continues until the user guesses the number correctly.
+Requirements:
+Random Number Generation: The program should generate a random number within a given range (e.g., 1 to 10). You can use the random.randint(1, 10) method, after including in your code import random
+User Input: Prompt the user to enter their guess. Ensure, the user guess is in the range, and if not, display "Your guess is out of bounds."
+Feedback to User: After each guess, the program should provide feedback: 
+If the guess is too high, display "Too hight!"
+If the guess is too low, display "Too low!"
+If the guess is correct, display "Congratulations! You guessed it right."
+Repeat Guesses: The game should continue, allowing the user to guess again if the guess is incorrect.
+Terminate the Game: Once the correct number is guessed, the game should end with message "Bravo, you guessed my number"
+Optional - Guess Counter: Keep track of the number of guesses the user makes and display this count when the correct number is guessed.
+Bonus Challenge:
+Add a feature to limit the number of guesses a user can make. If the user doesn't guess the number within the limit, end the game with a message indicating that the user has lost.
+Implement a difficulty level for the game (e.g., easy, medium, hard), where each level has a different range of numbers or a different number of allowed guesses.
+'''
+
+import random
+def rules():
+    print(f"{50*"$":^50}")
+    print(f"${"Welcome to Guess the number!":^48}$")
+    print(f"{50*"$":^50}")
+    print(f"${"Rules:":^48}$")
+    print(f"${"Only guess in range of numbers":^48}$")
+    print(f"${"Fixed number of guesses based on level":^48}$")
+    print(f"${"Really not that many rules, just have fun!":^48}$")
+    print(f"{50*"$":^50}")
+    
+
+def choose_level():
+    levels: [easy, medium, hard, extreme, borderline guessing]
+
+def generating_random_number():
+    number=random.randint(1,10)
+    return number
+
+rules()
